f1440e972b9361ca7bab8c621af7b640d780b5f5
neto <uoct50@gmail.com>
2026-08-16 11:21:38 -0300

    divisao das chunks e geracao dos embeddings feita com sucesso

--- app/main.py
+++ app/main.py
@@ -1,5 +1,28 @@
+import os
 from pdf_reader import ler_pdfs
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from google import genai
+from dotenv import load_dotenv
 
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client()
 
 caminho = "docs"
 texto_completo = ler_pdfs(caminho)
+texto_cortado = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200
+)
+chunks = texto_cortado.split_text(texto_completo)
+
+vetores = []
+
+for chunk in chunks:
+    resposta = client.models.embed_content(
+        model='gemini-embedding-001',
+        contents=chunk
+    )
+
+    vetores.append(resposta.embeddings[0].values)
